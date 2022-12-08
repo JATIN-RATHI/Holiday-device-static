1fc0bdf6ebde713b2421f583d4911b4a2b770273
ijatinrathi <lrathi@brinkshome.com>
2022-12-09 00:19:04 +0530

    updated UI with all links

---
+++ pages/404.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="text-center">
+        <img src="https://images.ctfassets.net/8053dpll6ke8/uaYVcV7xrlFA2ROH55Y8g/fc4e34d84a75e5c812cc5bf2e473ad80/404-error-holidaydevice.gif?h=250" alt="HolidayDevice 404Gif">
+        <h3><i> Uhh.. oh.. Page not found!</i></h3>
+        <h4 class="justify-content-center"><a href="/">Go to home page</a></h4>
+    </div>
+</template>
--- pages/Destinations/place/_package/_placePkg.vue
+++ pages/Destinations/place/_package/_placePkg.vue
@@ -1,65 +1,148 @@
 <template>
-    <div>
-        <NavbarHome />
-        <div class="container border-bottom mb-4">
-            <h1>{{ dataWithSlug.fields.destinationName }}</h1>
-            <p class="mb-4">
-                <FontAwesomeIcon class="icon-tour" icon="fa-calendar-days" />
-                {{ dataWithSlug.fields.destinationDuration }}                
-            </p>
+  <div>
+    <NavbarHome />
+    <div class="container" id="itineraryContent">
+      <div class="border-bottom mb-4">
+        <h1>{{ dataWithSlug.fields.destinationName }}</h1>
+        <p class="mb-4">
+          <FontAwesomeIcon class="icon-tour" icon="fa-calendar-days" />
+          {{ dataWithSlug.fields.destinationDuration }}
+        </p>
+      </div>
+      <div class="hero-package">
+        <img
+          :src="dataWithSlug.fields.destinationImage.fields.file.url"
+          :alt="dataWithSlug.fields.destinationImage.fields.file.title"
+        />
+      </div>
+      <div>
+        <div class="stickey">
+          <ul class="toggler-cls d-flex">
+            <li class="active toggler" id="overviewToggle">
+              <a
+                @click="
+                  scrollToSection('overview'), addClassActive('overviewToggle')
+                "
+                >Overview</a
+              >
+            </li>
+            <li class="toggler" id="itineraryToggle">
+              <a
+                @click="
+                  scrollToSection('itinerary'),
+                    addClassActive('itineraryToggle')
+                "
+                >Itinerary</a
+              >
+            </li>
+            <li class="toggler" id="detailPriceToggle">
+              <a
+                @click="
+                  scrollToSection('detailPrice'),
+                    addClassActive('detailPriceToggle')
+                "
+                >Detailed Price</a
+              >
+            </li>
+            <li class="toggler" id="tourInfoToggle">
+              <a
+                @click="
+                  scrollToSection('tourInfo'), addClassActive('tourInfoToggle')
+                "
+                >Tour Information</a
+              >
+            </li>
+            <li>
+                <a :href="dataWithSlug.fields.itineraryPdf.fields.file.url" target="blank">
+            <FontAwesomeIcon icon="fa-print" />
+          </a>
+            </li>
+          </ul>
         </div>
-        <div class="container hero-package">
-            <img :src="dataWithSlug.fields.destinationImage.fields.file.url" :alt="dataWithSlug.fields.destinationImage.fields.file.title">
+        <div id="overview" class="border-bottom">
+          <h2 class="heading-itinerary">Overview</h2>
+          <p>{{ dataWithSlug.fields.overview }}</p>
         </div>
-        <div class="container">
-            <div class="stickey">
-                <ul class="toggler-cls d-flex">
-                    <li class="active toggler" id="overviewToggle"><a @click="scrollToSection('overview'), addClassActive('overviewToggle')">Overview</a></li>
-                    <li class="toggler" id="itineraryToggle"><a @click="scrollToSection('itinerary'), addClassActive('itineraryToggle')">Itinerary</a></li>
-                    <li class="toggler" id="detailPriceToggle"><a @click="scrollToSection('detailPrice'), addClassActive('detailPriceToggle')">Detailed Price</a></li>
-                    <li class="toggler" id="tourInfoToggle"><a @click="scrollToSection('tourInfo'), addClassActive('tourInfoToggle')">Tour Information</a></li>
-                </ul>
+        <div id="itinerary" class="border-bottom">
+          <h2 class="heading-itinerary">Itinerary</h2>
+          <iframe
+            width="100%"
+            height="300"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Holiday%20Device,%20H%20No-126,%20Kadipur,%20Kushak%20Colony,%20Kadipur,%20Delhi,%20110036+(Holiday%20Device)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          >
+            <a href="https://www.maps.ie/distance-area-calculator.html"
+              >measure acres/hectares on map</a
+            >
+          </iframe>
+          <div
+            v-for="dayPath in dataWithSlug.fields.itinerarySection"
+            :key="dayPath.fields.id"
+            class="card-itinerary p-2"
+          >
+            <div id="dayItinerary">
+              <h4>{{ dayPath.fields.dayCount }}</h4>
             </div>
-            <div id="overview" class="border-bottom">
-                <h2 class="heading-itinerary">Overview</h2>
-                <p>{{ dataWithSlug.fields.overview }}</p>
+            <img
+              class="loc-gif"
+              src="https://images.ctfassets.net/8053dpll6ke8/2K3w7t3LEfciIxQ40Wk7El/fc1a61bdff902f2cf362180f5fad16cc/giphy.gif?h=250"
+              alt="LocGif"
+            />
+            <div class="details">
+              <h4 class="heading-itinerary">
+                {{ dayPath.fields.packageName }}
+              </h4>
+              <b-collapse
+                v-if="dayPath.fields.isActive"
+                visible
+                :id="'collapse-' + dayPath.fields.id"
+              >
+                <p class="paraBorder mb-0">
+                  {{ dayPath.fields.dayDescription }}
+                </p>
+                <img
+                  v-if="dayPath.fields.packageImage"
+                  class="dayPath-img paraBorder"
+                  :src="dayPath.fields.packageImage.fields.file.url"
+                  :alt="dayPath.fields.packageImage.fields.file.title"
+                />
+              </b-collapse>
+              <b-collapse v-else :id="'collapse-' + dayPath.fields.id">
+                <p class="paraBorder mb-0">
+                  {{ dayPath.fields.dayDescription }}
+                </p>
+              </b-collapse>
             </div>
-            <div id="itinerary" class="border-bottom">
-                <h2 class="heading-itinerary">Itinerary</h2>
-                <iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-                    src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Holiday%20Device,%20H%20No-126,%20Kadipur,%20Kushak%20Colony,%20Kadipur,%20Delhi,%20110036+(Holiday%20Device)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-                    <a href="https://www.maps.ie/distance-area-calculator.html">measure acres/hectares on map</a>
-                </iframe>
-                <div v-for="dayPath in dataWithSlug.fields.itinerarySection" :key="dayPath.fields.id" class="card-itinerary p-2">
-                    <div id="dayItinerary"><h4>{{ dayPath.fields.dayCount }}</h4></div>
-                    <img class="loc-gif" src="https://images.ctfassets.net/8053dpll6ke8/2K3w7t3LEfciIxQ40Wk7El/fc1a61bdff902f2cf362180f5fad16cc/giphy.gif?h=250" alt="LocGif">
-                    <div class="details">                        
-                        <h4 class="heading-itinerary">{{ dayPath.fields.packageName }}</h4>
-                        <b-collapse v-if="dayPath.fields.isActive" visible :id="'collapse-' + dayPath.fields.id">
-                            <p class="paraBorder mb-0">{{ dayPath.fields.dayDescription }}</p>
-                            <img v-if="dayPath.fields.packageImage" class="dayPath-img paraBorder" :src="dayPath.fields.packageImage.fields.file.url" :alt="dayPath.fields.packageImage.fields.file.title">
-                        </b-collapse>
-                        <b-collapse v-else :id="'collapse-' + dayPath.fields.id">
-                            <p class="paraBorder mb-0">{{ dayPath.fields.dayDescription }}</p>
-                        </b-collapse>
-                    </div>
-                    <a v-b-toggle="'collapse-' + dayPath.fields.id" class="btn-collapse"><FontAwesomeIcon icon="fa-down-left-and-up-right-to-center" /></a>
-                </div>                
-            </div>
-            <div id="detailPrice" class="border-bottom">
-                <h2 class="heading-itinerary">Detailed Price</h2>
-                <p class="justify-content-center my-4" v-html="richtextToHTML(dataWithSlug.fields.detailedPriceTable)"></p>
-            </div>
-            <div id="tourInfo" class="border-bottom">
-                <h2 class="heading-itinerary">Tour Information</h2>
-                <b-tabs>
-                    <b-tab title="Inclusions" active><p v-html="richtextToHTML(dataWithSlug.fields.inclusions)"></p></b-tab>
-                    <b-tab title="Exclusions"><p v-html="richtextToHTML(dataWithSlug.fields.exclusions)"></p></b-tab>
-                </b-tabs>
-            </div>
+            <a v-b-toggle="'collapse-' + dayPath.fields.id" class="btn-collapse"
+              ><FontAwesomeIcon icon="fa-down-left-and-up-right-to-center"
+            /></a>
+          </div>
         </div>
-        <Footerbase />
+        <div id="detailPrice" class="border-bottom">
+          <h2 class="heading-itinerary">Detailed Price</h2>
+          <p
+            class="justify-content-center my-4"
+            v-html="richtextToHTML(dataWithSlug.fields.detailedPriceTable)"
+          ></p>
+        </div>
+        <div id="tourInfo" class="border-bottom">
+          <h2 class="heading-itinerary">Tour Information</h2>
+          <b-tabs>
+            <b-tab title="Inclusions" active
+              ><p v-html="richtextToHTML(dataWithSlug.fields.inclusions)"></p
+            ></b-tab>
+            <b-tab title="Exclusions"
+              ><p v-html="richtextToHTML(dataWithSlug.fields.exclusions)"></p
+            ></b-tab>
+          </b-tabs>
+        </div>
+      </div>
     </div>
+    <Footerbase />
+  </div>
 </template>
 <script>
 import { Vue, Component } from "vue-property-decorator";
@@ -68,8 +151,9 @@ import Footerbase from "~/components/Footerbase.vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
-import { BLOCKS, MARKS } from '@contentful/rich-text-types'
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
+import { jsPDF } from "jspdf";
 import ContentfulRequests from "~/plugins/contentfulRequest";
 import $ from "jquery";
 
@@ -79,13 +163,17 @@ const options = {
     [MARKS.BOLD]: (text) => `<b>${text}</b>`,
   },
   renderNode: {
-    [BLOCKS.UL_LIST]: (node, next) => `<ul class="richText-cls" style="list-style-type: disc;padding-left: 8%">${next(node.content)}</ul>`,
+    [BLOCKS.UL_LIST]: (node, next) =>
+      `<ul class="richText-cls" style="list-style-type: disc;padding-left: 8%">${next(
+        node.content
+      )}</ul>`,
     [BLOCKS.LIST_ITEM]: (node, next) => `<li>${next(node.content)}</li>`,
-    [BLOCKS.PARAGRAPH]: (node, next) => `<p style="word-break:break-word">${next(node.content)}</p>`,
+    [BLOCKS.PARAGRAPH]: (node, next) =>
+      `<p style="word-break:break-word">${next(node.content)}</p>`,
     [BLOCKS.EMBEDDED_ASSET]: (node) =>
       `<img class="img-fluid" src="${node.data.target.fields.file.url}" height="${node.data.target.fields.file.details.image.height}" width="${node.data.target.fields.file.details.image.width}" alt="${node.data.target.fields.description}"/>`,
   },
-}
+};
 
 library.add(fas);
 @Component({
@@ -94,70 +182,84 @@ library.add(fas);
     FontAwesomeIcon,
     Footerbase,
   },
-  data(){
+  data() {
     return {
-        content: '',
-    }
+      content: "",
+    };
   },
   methods: {
     scrollToSection(sectionNo) {
-      const section = document.getElementById(sectionNo)
+      const section = document.getElementById(sectionNo);
       section?.scrollIntoView({
         behavior: "smooth",
         block: "center",
       });
     },
     addClassActive(toggleId) {
-      const toggle = document.getElementById(toggleId)
-      this.cancelActive()
-      $(toggle).addClass("active")
+      const toggle = document.getElementById(toggleId);
+      this.cancelActive();
+      $(toggle).addClass("active");
     },
     cancelActive() {
       $(".toggler").removeClass("active");
     },
     richtextToHTML(content) {
-        return documentToHtmlString(content, options)
-    }
+      return documentToHtmlString(content, options);
+    },
   },
   async asyncData({ params }) {
-    const dataWithSlug = (await contentfulRequest.getPageDataByPackageSlug(params.placePkg)) || {}
-    return { dataWithSlug }
-  }
+    const dataWithSlug =
+      (await contentfulRequest.getPageDataByPackageSlug(params.placePkg)) || {};
+    return { dataWithSlug };
+  },
+  mounted() {
+    const doc = new jsPDF('p', 'mm', [297, 210]);
+
+    $("#downloadBtn").click(function () {
+      doc.html(document.body, {
+        callback: function (doc) {
+          doc.save('HolidayDeviceItinerary.pdf');
+        },
+        x: 15,
+        y: 15,
+      });
+    });
+  },
 })
 export default class PlacePackage extends Vue {}
 </script>
 <style>
 .richText-cls li {
-    font-weight: 400;
+  font-weight: 400;
 }
 .hide {
-    display: none !important;;
+  display: none !important;
 }
 .show {
-    display: block !important;
+  display: block !important;
 }
 .btn-collapse {
-    color: #f34848;
+  color: #f34848;
 }
 .card-itinerary {
-    display: flex;
+  display: flex;
 }
 .icon-tour {
-    width: 25px;
-    height: 22px;
+  width: 25px;
+  height: 22px;
 }
 .hero-package img {
   width: 100%;
 }
 .heading-itinerary {
-    color: #343a40;
+  color: #343a40;
 }
 .btn-collapse {
-    cursor: pointer;
+  cursor: pointer;
 }
 .stickey {
-    position: sticky;
-    top: 0;
+  position: sticky;
+  top: 0;
 }
 .toggler-cls {
   list-style: none;
@@ -168,7 +270,7 @@ export default class PlacePackage extends Vue {}
   overflow-x: scroll;
 }
 .toggler-cls::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
 
 .toggler-cls li {
@@ -185,35 +287,35 @@ export default class PlacePackage extends Vue {}
 }
 
 #dayItinerary {
-    width: 100%;
+  width: 100%;
 }
 .details {
-    width: 235%;
+  width: 235%;
 }
-.details h4{
-    padding-left: 11%;
+.details h4 {
+  padding-left: 11%;
 }
 .paraBorder::before {
-    content: '';
-    padding: 7%;
-    margin-left: -25px;
-    border-left: 2px dashed #c56868f0;
+  content: "";
+  padding: 7%;
+  margin-left: -25px;
+  border-left: 2px dashed #c56868f0;
 }
 .loc-gif {
-    width: 50px;
-    height: 50px;
-    margin-top: -15px;
+  width: 50px;
+  height: 50px;
+  margin-top: -15px;
 }
 .dayPath-img {
-    width: 80%;
-    margin-left: 11%;
+  width: 80%;
+  margin-left: 11%;
 }
 @media (min-width: 768px) {
-    .dayPath-img {
-        width: 50%;
-    }
-    .toggler-cls li {
-        font-size: 1.7rem;
-    }
+  .dayPath-img {
+    width: 50%;
+  }
+  .toggler-cls li {
+    font-size: 1.7rem;
+  }
 }
 </style>
